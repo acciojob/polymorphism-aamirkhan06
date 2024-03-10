1e898d6da4b6063628a5fe15cd953aa252a1e790
aamirkhan06 <aamir.campus@gmail.com>
2024-03-10 18:34:17 +0530

    modified commit changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
-public class Main {
+public class Main
+{
+    static class Product
+    {
+        public int product(int x, int y)
+        {
+            return x*y;
+        }
+        public int product(int x, int y, int z)
+        {
+            return x*y*y;
+        }
+        public double product(double x, double y)
+        {
+            return x*y;
+        }
+    }
 
+    public static void main(String[] args)
+    {
+        Product p=new Product();
+        System.out.println(p.product(10,10));
+        System.out.println(p.product(10,10,10));
+        System.out.println(p.product(10.0,10.0));
+    }
 }
